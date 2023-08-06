7112e3c2ee3e0a1e090677b5ba551d5eb54f46e1
Manvir <mkaur5_be20@thapar.edu>
2023-08-06 18:11:34 +0530

    _

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Button from './components/Button';
+import Editor from './components/Editor';
+
+function App() {
+
+  const [openedEditor, setOpenedEditor] = useState('html');
+
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+  const [srcDoc, setSrcDoc] = useState(``);
+
+
+  const onTabClick = (editorName) => {
+    setOpenedEditor(editorName);
+  };
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setSrcDoc(
+        `
+          <html>
+            <body>${html}</body>
+            <style>${css}</style>
+            <script>${js}</script>
+          </html>
+        `
+      )
+    }, 250);
+
+    return () => clearTimeout(timeOut)
+  }, [html, css, js])
+
+  return (
+    <div className="App">
+      <p>Welcome to the editor</p>
+      <div className="tab-button-container">
+        <Button class ="button" title="HTML" onClick={() => {
+          onTabClick('html')
+        }} />
+        <Button class ="button" title="CSS" onClick={() => {
+          onTabClick('css')
+        }} />
+        <Button class ="button" title="JavaScript" onClick={() => {
+          onTabClick('js')
+        }} />
+      </div>
+      <div className="editor-container">
+        {
+          openedEditor === 'html' ? (
+            <Editor
+              language="xml"
+              displayName="HTML"
+              value={html}
+              setEditorState={setHtml}
+            />
+          ) : openedEditor === 'css' ? (
+            <Editor
+              language="css"
+              displayName="CSS"
+              value={css}
+              setEditorState={setCss}
+            />
+          ) : (
+            <Editor
+              language="javascript"
+              displayName="JS"
+              value={js}
+              setEditorState={setJs}
+            />
+          )
+        }
+      </div>
+      <div>
+        <iframe
+          id="my_iframe"
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="1"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Editor.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/theme/mdn-like.css';
+import 'codemirror/theme/the-matrix.css';
+import 'codemirror/theme/night.css';
+
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+
+import { Controlled as ControlledEditorComponent } from 'react-codemirror2';
+
+
+
+const Editor = ({ language, value, setEditorState }) => {
+
+  const [theme, setTheme] = useState("dracula")
+  const handleChange = (editor, data, value) => {
+    setEditorState(value);
+  }
+
+  const themeArray = ['dracula', 'material', 'mdn-like', 'the-matrix', 'night']
+
+  return (
+    <div className="editor-container">
+      <div style={{marginBottom: "10px"}}>
+        <label for="cars">Choose a theme: </label>
+        <select name="theme" onChange={(el) => {
+          setTheme(el.target.value)
+        }}>
+          {
+            themeArray.map( theme => (
+              <option value={theme}>{theme}</option>
+            ))
+          }
+        </select>
+      </div>
+      <ControlledEditorComponent
+        onBeforeChange={handleChange}
+        value= {value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          theme: theme,
+          autoCloseTags: true,
+          autoCloseBrackets: true, 
+        }}
+      />
+    </div>
+  )
+}
+
+export default Editor
